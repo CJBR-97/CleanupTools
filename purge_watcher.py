ca8790d45172084129c2c5bf69139ed5d9516599
Christoff Reimer <69945487+CJBR-97@users.noreply.github.com>
2023-10-13 17:30:41 -0400

    Create purge_watcher.py

---
+++ purge_watcher.py
@@ -0,0 +1,43 @@
+import re
+import pandas as pd
+
+def purge_watcher(path_purge_file, path_out_file):
+  if not type(path_purge_file) is str or not type(path_out_file) is str:
+    raise TypeError("Only string paths are allowed")
+  text_file = open(path_purge_file, "r")
+  filename = text_file.readlines()
+  filetype = []
+  filepath = []
+
+  # Regex commands to clean up data (Stackexchange used to find correct syntax)
+  # These terms must be adjusted to fit your directory structure, these ones are set up for Niagara
+  regex = '.*?/gpfs/.*/(.*)'
+  regex2 = '(\..*)'
+  regex3 = '.*?(/gpfs/.*)'
+
+  # Use regex to get the file names, type/extension, and path in directory
+  for i, l in enumerate(filename):
+    filepath.append(re.findall(regex3, l)[0])
+    filename[i] = re.findall(regex, l)[0]
+    filetype.append(re.findall(regex2, filename[i])[0])
+  text_file.close()
+
+  # Ensure extraction did not pull inconsistent number of names, types, or paths
+  if any(len(x) != len(filename) for x in [filename, filetype, filepath]):
+    raise ValueError("Number of files, filetypes, or filepaths found do not match each other")
+
+  print("Purge list processed, detected {} files".format(len(filepath)))
+
+  # Format dict for pandas use
+  data = {
+    "filename": filename,
+    "filetype": filetype,
+    "filepath": filepath
+  }
+
+  #load data into a DataFrame object:
+  out_data = pd.DataFrame(data)
+  print("Dataframe has shape", out_data.shape)
+  out_data.sort_values(by=["filetype", "filename"], inplace = True, ignore_index = True)
+  print(out_data.head(5))
+  out_data.to_csv(path_out_file)
